4d98ff80d8ff76410f4c34c017c81a2f81864377
James Lee <jml@jmlzone.com>
2017-12-29 17:33:49 -0500

    more documentation

--- setup.sh
+++ setup.sh
@@ -4,15 +4,16 @@
 # running PiPtr
 echo "Updating and upgrading"
 sudo apt-get update
-sudo apt-get upgrade
-echo "----------------------------------------------------------------------\n"
+sudo apt-get upgrade -y
+echo "----------------------------------------------------------------------"
+
 echo " installing needed packages"
-sudo apt-get install -y python-dev python-pyaudio libasound2-dev espeak python3-smbus scons python-dev swig raspi-gpio
-echo "----------------------------------------------------------------------\n"
+sudo apt-get install -y python-dev python-pyaudio libasound2-dev espeak python3-smbus scons python-dev swig raspi-gpio emacs telnet
+echo "----------------------------------------------------------------------"
 
 echo " installing python3 alsa audio"
 sudo -H pip3 install pyalsaaudio
-echo "----------------------------------------------------------------------\n"
+echo "----------------------------------------------------------------------"
 
 echo "installing pixel driiver"
 cd ~
@@ -22,11 +23,15 @@ scons
 cd python
 python3 ./setup.py build
 sudo python3 ./setup.py install
-echo "----------------------------------------------------------------------\n"
+echo "----------------------------------------------------------------------"
 
 echo "installing dht driiver"
 cd ~
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 sudo python3 setup.py install
-echo "----------------------------------------------------------------------\n"
+echo "----------------------------------------------------------------------"
+
+
+echo "done"
+
