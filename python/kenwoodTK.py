a588382942b0c311531b9286415b9c9af9a4e491
James Lee <jml@jmlzone.com>
2020-01-04 19:54:23 -0500

    added pl board control

--- python/kenwoodTK.py
+++ python/kenwoodTK.py
@@ -59,19 +59,95 @@ BLINKEN   = 1       # Enable two Phase Blinking
 
 # Intestsity Registers
 INTSTATIC = 0xff   # all intensities static
+""" MCP23008 Register names
+"""
+IODIR    = 0
+IPOL     = 1
+GPINTEN  = 2
+DEFVAL   = 3
+INTCON   = 4
+IOCON    = 5
+GPPU     = 6
+INTF     = 7
+INTCAP   = 8
+GPIOR    = 9
+OLAT     = 10
+# HWIO directions
+INPUT = 1
+OUTPUT = 0
+OFF = 1
+ON = 0
+
+#frequency table
+plSwitchTable = {
+    "67.0" : [OFF,ON ,OFF,OFF,OFF,ON ],
+    "71.9" : [ON ,ON ,OFF,OFF,OFF,ON ],
+    "74.4" : [OFF,OFF,ON ,OFF,OFF,ON ],
+    "77.0" : [ON ,OFF,ON ,OFF,OFF,ON ],
+    "79.7" : [OFF,ON ,ON ,OFF,OFF,ON ],
+    "82.5" : [ON ,ON ,ON ,OFF,OFF,ON ],
+    "85.4" : [OFF,OFF,OFF,ON ,OFF,ON ],
+    "88.5" : [ON ,OFF,OFF,ON ,OFF,ON ],
+    "91.5" : [OFF,ON ,OFF,ON ,OFF,ON ],
+    "94.8" : [OFF,ON ,ON ,OFF,OFF,OFF],
+    "100.0" : [ON ,ON ,ON ,OFF,OFF,OFF],
+    "103.5" : [OFF,OFF,OFF,ON ,OFF,OFF],    
+    "107.2" : [ON ,OFF,OFF,ON ,OFF,OFF],
+    "110.9" : [OFF,ON ,OFF,ON ,OFF,OFF],
+    "114.8" : [ON ,ON ,OFF,ON ,OFF,OFF],
+    "118.8" : [OFF,OFF,ON ,ON ,OFF,OFF],
+    "123.0" : [ON ,OFF,ON ,ON ,OFF,OFF],
+    "127.3" : [OFF,ON ,ON ,ON ,OFF,OFF],
+    "131.8" : [ON ,ON ,ON ,ON ,OFF,OFF],
+    "136.5" : [OFF,OFF,OFF,OFF,ON, OFF],
+    "141.3" : [ON ,OFF,OFF,OFF,ON ,OFF],
+    "146.2" : [OFF,ON ,OFF,OFF,ON ,OFF],
+    "151.4" : [ON ,ON ,OFF,OFF,ON ,OFF],
+    "156.7" : [OFF,OFF,ON ,OFF,ON ,OFF],
+    "162.2" : [ON ,OFF,ON ,OFF,ON ,OFF],
+    "167.9" : [OFF,ON ,ON ,OFF,ON ,OFF],
+    "173.8" : [ON ,ON ,ON ,OFF,ON ,OFF],
+    "179.9" : [OFF,OFF,OFF,ON ,ON ,OFF],
+    "186.2" : [ON ,OFF,OFF,ON ,ON ,OFF],
+    "192.8" : [OFF,ON ,OFF,ON ,ON ,OFF],
+    "203.5" : [ON ,ON ,OFF,ON ,ON ,OFF],
+    "210.7" : [OFF,OFF,ON ,ON ,ON ,OFF],
+    "218.1" : [ON ,OFF,ON ,ON ,ON ,OFF],
+    "225.7" : [OFF,ON ,ON ,ON ,ON ,OFF],
+    "233.6" : [ON ,ON ,ON ,ON ,ON ,OFF],
+    "241.8" : [OFF,OFF,OFF,OFF,OFF,ON ],
+    "250.3" : [ON ,OFF,OFF,OFF,OFF,ON ]
+    }
+        
 class kenwoodTK:
     """ A Kenwood TK701 (VHF) or Kenwood TK801 (UHF) With the proms removed and
     a max7314 driging the prom outputs to be frequency agile 
     """
-    def __init__ (self,addr=ADDR2) :
+    def __init__ (self,addr=ADDR2,plAddr=0x27) :
         self.addr = addr
+        self.plAddr = plAddr # MCP3008 for frequency control
         self.i2cBus = smbus.SMBus(1)
+        self.plVal=dict()
+        self.SwitchTableToValue()
         """ initialize max7314 for static outputs,
             txrx switching with the blink input 
         """
-        self.i2cBus.write_i2c_block_data(self.addr,CONFIG_L,[ALLOUT,ALLOUT]);
-        self.i2cBus.write_byte_data(self.addr,MASTERO16,(MASTERSTATIC |  O16STATIC))
-        self.i2cBus.write_byte_data(self.addr,CONFIG,(O16P0 |  GLOBINTEN |  BLINKEN))
+        try:
+            self.i2cBus.write_i2c_block_data(self.addr,CONFIG_L,[ALLOUT,ALLOUT]);
+            self.i2cBus.write_byte_data(self.addr,MASTERO16,(MASTERSTATIC |  O16STATIC))
+            self.i2cBus.write_byte_data(self.addr,CONFIG,(O16P0 |  GLOBINTEN |  BLINKEN))
+            self.freqCtl = True
+        except:
+            print("Kenwood TK frequency control is offline");
+            self.freqCtl = False
+        try:
+            self.i2cBus.write_byte_data(self.plAddr, IODIR,0) # port as output
+            self.plCtl = True
+            self.setPlByName("88.5")
+        except:
+            print("Kenwood TK PL control is offline");
+            self.plCtl = False
+           
 
     def setFreqDirect(self,tx_freq, rx_freq) :
         if(tx_freq < 200000) :
@@ -106,3 +182,22 @@ class kenwoodTK:
         buf0 = self.i2cBus.read_byte_data(self.addr,CONFIG)
         print("Configuration is %x" % buf0)
 
+    def SwitchTableToValue (self):
+        for x in plSwitchTable:
+            a = plSwitchTable[x]
+            v = 0
+            for i in range(6):
+                v = v + (a[i]<<i)
+                self.plVal[x] = v
+
+    def setPlByName(self,plName) :
+        if(plName in self.plVal):
+            if(self.plCtl) :
+                try:
+                    self.i2cBus.write_byte_data(self.plAddr, GPIOR, self.plVal[plName])
+                except:
+                    print("unexpected I2C error on PL write")
+            else :
+                print("There is not PLcontrol for theis kenwood")
+        else :
+            print("I dont know a code for PL %s" %plName)
