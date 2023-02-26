c221f431fafde750b9e67d782c67fee8d0b47956
James Lee <jml@jmlzone.com>
2023-02-26 10:20:00 -0500

    add notes

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# This can be auto run when the pi starts by addiing to
+# ~/.config/lxsession/LXDE-pi/autostart
+# ie:
+# @lxpanel --profile LXDE-pi
+# @pcmanfm --desktop --profile LXDE-pi
+# @xscreensaver -no-splash
+# @point-rpi
+# @lxterminal
+# @lxterminal --command /home/pi/PiPtr/start.sh
+#
 if [ $DISPLAY = ":0" ]; then
    echo "On console, starting PiPtr in 30 seconds"
    sleep 30
