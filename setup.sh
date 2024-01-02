64f92a1d5f5989ae2d8c3a91aca6d9b349376dfd
James Lee <jml@jmlzone.com>
2024-01-02 09:39:17 -0500

    bookworm updates

--- setup.sh
+++ setup.sh
@@ -8,24 +8,26 @@ sudo apt upgrade -y
 echo "----------------------------------------------------------------------"
 
 echo " installing needed packages"
-sudo apt install -y python-dev python-pyaudio libasound2-dev espeak \
-     python3-smbus scons python-dev swig raspi-gpio emacs telnet \
+sudo apt install -y python3-dev python3-pyaudio libasound2-dev espeak-ng \
+     python3-smbus scons python3-dev swig raspi-gpio emacs telnet \
      cmake automake libtool alsa-utils alsaplayer-alsa libasound2 \
      libasound2-dev python3-alsaaudio python3-pyalsa
 echo "----------------------------------------------------------------------"
 
-echo " installing python3 alsa audio"
-sudo -H pip3 install pyalsaaudio
-echo "----------------------------------------------------------------------"
+#echo " installing python3 alsa audio"
+#sudo -H pip3 install pyalsaaudio
+#echo "----------------------------------------------------------------------"
 
 echo "installing pixel driiver"
 cd ~
 git clone https://github.com/jgarff/rpi_ws281x
-cd rpi_ws281x/
-scons
-cd python
+git clone https://github.com/rpi-ws281x/rpi-ws281x-python
+cd rpi-ws281x-python/library
+rmdir lib
+ln -s ../../rpi_ws281x lib
 python3 ./setup.py build
 sudo python3 ./setup.py install
+#sudo chmod a+rw /dev/mem
 echo "----------------------------------------------------------------------"
 
 echo "installing dht driver"
@@ -35,7 +37,7 @@ cd Adafruit_Python_DHT
 sudo python3 setup.py install
 echo "----------------------------------------------------------------------"
 
-echo "getting updated espreak with asla interface:"
+echo "getting updated espeak with asla interface:"
 cd ~
 git clone https://github.com/jmlzone/espeak-ng
 cd espeak-ng/
